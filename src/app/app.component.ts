cfd8b580db4351703aad5fc04237dca791c821cb
Ahmed Zaki <ahmed.zaki@thoughtdesign.com.au>
2019-11-19 19:48:22 +0200

    remove missing file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { ToastNotification } from './toast-notification/model/notification';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   title = 'discovergy-task';
